d57b51a5fc3754973fa5b6167568e55e1ffdb326
bhoomika25bca <bhoomika1045@gmail.com>
2026-03-29 14:49:52 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,10 +5,9 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: "*"
-}));
-app.use(express.json());
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port", PORT);
+});
 
 // ✅ MySQL connection using DB_URL
 const db = mysql.createConnection(process.env.DB_URL);
